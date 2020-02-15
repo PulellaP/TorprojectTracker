2835de392cb7deb64297887dcd91582c4580fcd5
PulellaP <ppulella@gmail.com>
2020-02-14 19:47:21 -0500

    Created a tuple of tuples with 20 entries

--- fetch_and_deposit.py
+++ fetch_and_deposit.py
@@ -10,11 +10,18 @@ def get_obj(endpoint, listnum):
     json_python_obj = json.loads(string)
     return(json_python_obj[listnum])
 
-
-
-
-def get_one_entry(json_single):
+def listify_one_entry(json_single):
     return(json_single["sha"], json_single["committer"]["login"], json_single["commit"]["committer"]["date"])
 
 entry_object = get_obj(request, 0)
-print(get_one_entry(entry_object))
+#print(listify_one_entry(entry_object))
+
+def loop_through_entries(url_request):
+    list_of_lists = ()
+    for i in range(21):
+        jsn_obj = get_obj(url_request, i)
+        listified = listify_one_entry(jsn_obj)
+        list_of_lists = (list_of_lists,  listified)
+    return(list_of_lists)
+
+print(loop_through_entries(request))
